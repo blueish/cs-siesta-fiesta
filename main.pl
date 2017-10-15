49dccb3151192bf53f3bbd885675fdae1c5977c1
Sam Chow <samchowa@gmail.com>
2017-10-15 14:03:01 -0700

    specify props better

--- main.pl
+++ main.pl
@@ -4,12 +4,11 @@
 % Graduation requirements
 graduated :-
 	communications,
-	arts,
-	remaining_fac,
+	arts, 
 	electives,
-	first_year_cpsc_reqs,
-	second_year_cpsc_reqs,
-	second_year_math_stats_reqs,
+	first_year_reqs, 
+	second_year_cpsc_reqs, 
+	second_year_math_stats_reqs, 
 	third_and_fourth_cpsc_reqs.
 
 
@@ -27,13 +26,17 @@ communications :-
 
 %% PROP TRIPLES
 
+% CPSC is the example for all of these prop vals
+
 % prop(courseID, number, X) is true when a course exists with X being the courseID
-% prop(courseID, completed, X) is true when a course has been completed with X being one of {true, false}
-% prop(courseID, credits, X) is true when a course is worth X credits
-% prop(courseID, satisfies_req, X) is true when a course satisfies part of the requirement of X
 prop(cpsc312, number, 312).
+
+% prop(courseID, completed, X) is true when a course has been completed with X being one of {true, false}
 prop(cpsc312, completed, false).
-prop(cpsc312, credits, 3).
+% prop(courseID, faculty, X) is true when a course is part of faculty X
+prop(cpsc312, faculty, scie). 
+
+% prop(courseID, satisfies_req, X) is true when a course satisfies part of the requirement of X
 prop(cpsc312, satisfies_req, third_and_fourth_cpsc_reqs).
 
 % prop(courseID, satisfies_req, true) is true when courseID satisfies the requirement for satisfies_req
@@ -54,4 +57,4 @@ prop(scie300, satisfies_req, communications).
 prop(apsc176, satisfies_req, communications).
 prop(astu100, satisfies_req, communications).
 prop(astu150, satisfies_req, communications).
-prop(wrds150, satisfies_req, communications).
+prop(wrds150, satisfies_req, communications). 
